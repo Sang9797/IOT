7420b2e6a191499605a19858532cf7c6f270edfe
sanglee97 <admin@example.com>
2025-10-25 14:37:32 +0700

    chore: update code to run mvn install successfully
    Signed-off-by: sanglee97 <admin@example.com>

--- analysis-report-service/src/main/java/com/iot/analysisreport/AnalysisReportApplication.java
+++ analysis-report-service/src/main/java/com/iot/analysisreport/AnalysisReportApplication.java
@@ -2,12 +2,10 @@ package com.iot.analysisreport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableKafka
 @EnableScheduling
 public class AnalysisReportApplication {
--- device-management-service/src/main/java/com/iot/devicemanagement/DeviceManagementApplication.java
+++ device-management-service/src/main/java/com/iot/devicemanagement/DeviceManagementApplication.java
@@ -2,11 +2,9 @@ package com.iot.devicemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableKafka
 public class DeviceManagementApplication {
 
